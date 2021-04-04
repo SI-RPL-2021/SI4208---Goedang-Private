cf31ff57166676ffc7ed9c36f01ad7eb8e43f622
nilamazzahra <nilameriaazzahra@student.telkomuniversity.ac.id>
2021-04-04 21:41:00 +0800

    Revert "update login akun ref #4"
    This reverts commit 57b6ce587992426776708bcb63748c96db23b74d.

--- login.php
+++ login.php
@@ -1,8 +1,17 @@
 <?php 
-session_start();
-include_once('db.php');
-$database = new database();
 
+ 
+if(isset($_SESSION['is_login']))
+{
+    header('location:home.php');
+}
+ 
+if(isset($_COOKIE['username']))
+{
+  $database->relogin($_COOKIE['username']);
+  header('location:home.php');
+}
+ 
 if(isset($_POST['login']))
 {
     $username = $_POST['username'];
@@ -18,7 +27,7 @@ if(isset($_POST['login']))
  
     if($database->login($username,$password,$remember))
     {
-      header('location:login.php');
+      header('location:home.php');
     }
 }
 ?>
@@ -28,8 +37,9 @@ if(isset($_POST['login']))
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content=" Bootstrap contributors">
-    <title>Login Form</title>
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Form login </title>
  
     <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sign-in/">
  
@@ -39,7 +49,7 @@ if(isset($_POST['login']))
  
     <style>
       .bd-placeholder-img {
-        font-size: 1rem;
+        font-size: 1.125rem;
         text-anchor: middle;
         -webkit-user-select: none;
         -moz-user-select: none;
@@ -47,74 +57,42 @@ if(isset($_POST['login']))
         user-select: none;
       }
  
-      @media (min-width: 500px) {
+      @media (min-width: 768px) {
         .bd-placeholder-img-lg {
-          font-size: 1.5rem;
+          font-size: 3.5rem;
         }
       }
-      #card {
-        background: #fbfbfb;
-        border-radius: 4px;
-        box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.30);
-        height: 470px;
-        margin: 3rem auto 5.1rem auto;
-        width: 350px;
-}
-      .satu {
-        font-size: 12px;
-        }
-      .dua {
-      font-size: 12px;
-       }
-       .tiga {
-        font-size: 14px;
-        }
-      .empat {
-      font-size: 14px;
-       }
     </style>
+    <!-- Custom styles for this template -->
     <link href="assets/css/signin.css" rel="stylesheet">
   </head>
-  <div id="card">
-  <div class="text-center">
-    <img class="" src="logonew.png" alt="" width="100" height="100">
-  <div class="text-left">
-  <form class="form-signin" method="post" action="">
-    <p class="font-weight-bold">Masuk<p>
-    <p class="tiga">Nomor HP atau email<p>
-    <label for="username" class="sr-only">Email</label>
-    <input type="text" id="username" class="form-control" placeholder="email@goedang.com" name="username" required autofocus>
-    
-    <p class="empat">Password<p>
-    <label for="password" class="sr-only">Password</label>
-    <input type="text" id="password" class="form-control" placeholder="Password" name="password" required>
-  <div class="dua">
+  <body class="text-center">
+    <form class="form-signin" method="post" action="">
+  <img class="mb-4" src="logingambar.png" alt="" width="100" height="100">
+  <h1 class="h3 mb-3 font-weight-normal">Masuk </h1>
+  <label for="username" class="sr-only">Nomor HP atau Email </label>
+  <input type="text" id="username" class="form-control" placeholder="Username" name="username" required autofocus>
+  <label for="password" class="sr-only">Password</label>
+  <input type="password" id="password" class="form-control" placeholder="Password" name="password" required>
+  <div class="checkbox mb-3">
     <label>
-      <input type="checkbox" value="remember-me" name="remember" class="dua"> Ingatkan Saya
+      <input type="checkbox" value="remember-me" name="remember"> Ingatkan Saya
     </label>
-    </br>
-  <div class="text-center">
-    <button class="btn btn-primary" type="submit" style.display='block'  name="login">Masuk</button>
-  </br>
-  </br>
-  <div class="text-left">
-    <p class="satu" style="text-muted" >Toko anda belum terdaftar di Goedang <a href="#"  class="w3-text-blue">Registrasi</a></p>
-  </form>
   </div>
-  </div>
-  </div>
-  </div>
-  </div>
-  </div>
+  <button class="btn btn-lg btn-primary btn-block" type="submit" name="login">Masuk </button>
+  
+</form>
 
 <style type="text/css">
+
 body {
-background: url(storage.jpg) no-repeat fixed;
+background: url(gudang.jpg) no-repeat fixed;
    -webkit-background-size: 100% 100%;
    -moz-background-size: 100% 100%;
    -o-background-size: 100% 100%;
    background-size: 100% 100%;
 }
+
 </style>
 
 
