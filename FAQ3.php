0c13ff5ee12e436fe5a2591d2861ea3bb4d6b0b7
Monferno <hanachmads@gmail.com>
2021-04-22 16:38:03 +0700

    update FAQ

---
+++ FAQ3.php
@@ -0,0 +1,145 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="css/footer.css" rel="stylesheet">
+    
+    <script src="https://kit.fontawesome.com/3dd6eb1413.js" crossorigin="anonymous"></script>
+
+    <title>FAQ Page</title>
+    <link rel="stylesheet" href="stylesfaq3.css">
+
+  </head>
+
+ <body>
+   <!-- start navbar -->
+   <body>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light shadow mb-5 bg-body rounded">
+        <div class="container-fluid d-flex justify-content-around">
+          <a class="navbar-brand" href="#">
+            <img src="Goedang.png" alt="" height="40">
+          </a>
+          <form class="w-50">
+            <div class="input-group border rounded-pill">
+              <input type="search" placeholder=" Cari Produk atau Nama Brand" aria-describedby="button-addon3" class="form-control bg-none border-0">
+              <div class="input-group-append border-0">
+                <button id="button-addon3" type="button" class="btn btn-link text-primary"><i class="fa fa-search"></i></button>
+              </div>
+            </div>
+          </form>
+          
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fas fa-clipboard-list"></i></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled mx-3" href="#">|</i></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Login</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Register</a>
+              </li>
+            </ul>
+
+          </div>
+        </nav>
+      </div>
+      <!-- end navbar -->
+
+<br><br><br><br>
+  <div class="container-fluid">
+    <h3>Frequently Asked Question(FAQs)</h3>
+    <div class="accordion">
+        <div class="icon"></div>
+        <h5>Apa itu GOEDANG?</h5>
+    </div>
+    <div class="panel">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt maxime explicabo voluptate sit rem atque voluptatibus earum velit, 
+            voluptas ratione. Praesentium consequatur placeat velit doloremque debitis, 
+            accusantium ratione iusto magni.</p>
+    </div>
+
+    <div class="accordion">
+        <div class="icon"></div>
+        <h5>Dimana data pengguna disimpan?</h5>
+    </div>
+    <div class="panel">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt maxime explicabo voluptate sit rem atque voluptatibus earum velit, 
+            voluptas ratione. Praesentium consequatur placeat velit doloremque debitis, 
+            accusantium ratione iusto magni.</p>
+    </div>
+
+    <div class="accordion">
+        <div class="icon"></div>
+        <h5>Bagaimaana GOEDANG bekerja?</h5>
+    </div>
+    <div class="panel">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt maxime explicabo voluptate sit rem atque voluptatibus earum velit, 
+            voluptas ratione. Praesentium consequatur placeat velit doloremque debitis, 
+            accusantium ratione iusto magni.</p>
+    </div>
+
+    <div class="accordion">
+        <div class="icon"></div>
+        <h5>Bagaimana cara melakukan pembayaran?</h5>
+    </div>
+    <div class="panel">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt maxime explicabo voluptate sit rem atque voluptatibus earum velit, 
+            voluptas ratione. Praesentium consequatur placeat velit doloremque debitis, 
+            accusantium ratione iusto magni.</p>
+    </div>
+    <div class="accordion">
+      <div class="icon"></div>
+      <h5>Apakah GOEDANG menyediakan jaminan garansi?</h5>
+  </div>
+  <div class="panel">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt maxime explicabo voluptate sit rem atque voluptatibus earum velit, 
+          voluptas ratione. Praesentium consequatur placeat velit doloremque debitis, 
+          accusantium ratione iusto magni.</p>
+  </div>
+
+  <div class="accordion">
+      <div class="icon"></div>
+      <h5>Apa fungsi point akun?</h5>
+  </div>
+  <div class="panel">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt maxime explicabo voluptate sit rem atque voluptatibus earum velit, 
+          voluptas ratione. Praesentium consequatur placeat velit doloremque debitis, 
+          accusantium ratione iusto magni.</p>
+  </div>
+
+  <div class="accordion">
+      <div class="icon"></div>
+      <h5>Bagaimana mengedit informasi akun?</h5>
+  </div>
+  <div class="panel">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt maxime explicabo voluptate sit rem atque voluptatibus earum velit, 
+          voluptas ratione. Praesentium consequatur placeat velit doloremque debitis, 
+          accusantium ratione iusto magni.</p>
+  </div>
+
+  <div class="accordion">
+      <div class="icon"></div>
+      <h5>Cabang GOEDANG ada dimana saja?</h5>
+  </div>
+  <div class="panel">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt maxime explicabo voluptate sit rem atque voluptatibus earum velit, 
+          voluptas ratione. Praesentium consequatur placeat velit doloremque debitis, 
+          accusantium ratione iusto magni.</p>
+  </div>
+</div>  
+
+<script src="app.js"></script>
+</body>
+</html>
