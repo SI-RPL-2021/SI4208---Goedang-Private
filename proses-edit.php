09f14e7a2a27d024f9ecc758aff4bce9aabb5b90
faris <farisaufar@gmail.com>
2021-04-30 10:07:40 +0700

    menyelesaikan edit akun profil ref #16

--- proses-edit.php
+++ proses-edit.php
@@ -1,29 +1,30 @@
 <?php
 
-include("config.php");
+require_once "config.php";
 
-// cek apakah tombol simpan sudah diklik atau blum?
+session_start();
+
+$id = $_SESSION['id'];
+
 if(isset($_POST['simpan'])){
-
-    // ambil data dari formulir
-    $id = $_POST['id'];
     $nama = $_POST['nama'];
     $nik = $_POST['nik'];
     $email = $_POST['email'];
     $notelp = $_POST['notelp'];
     $passwd = $_POST['passwd'];
+    $hashpass = password_hash($passwd, PASSWORD_DEFAULT);
     $nama_toko = $_POST['nama_toko'];
     $no_toko = $_POST['no_toko'];
     $alamat_toko = $_POST['alamat_toko'];
 
     // buat query update
-    $sql = "UPDATE user SET nama='$nama', nik='$nik', email='$email', notelp='$notelp', passwd='$passwd', nama_toko='$nama_toko', no_toko='$no_toko', alamat_toko='$alamat_toko' WHERE id=$id";
-    $query = mysqli_query($db, $sql);
+    $sql = "UPDATE user SET nama='$nama', nik='$nik', email='$email', notelp='$notelp', passwd='$hashpass', nama_toko='$nama_toko', no_toko='$no_toko', alamat_toko='$alamat_toko' WHERE id=$id";
+    $query = mysqli_query($link, $sql);
 
     // apakah query update berhasil?
     if( $query ) {
         // kalau berhasil alihkan ke halaman list-siswa.php
-        header('Location: list-siswa.php');
+        echo "<script>alert('Profil berhasil diperbaharui.');document.location='edit akun profil.php'</script>";
     } else {
         // kalau gagal tampilkan pesan
         die("Gagal menyimpan perubahan...");
