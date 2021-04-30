d064172b20aadbf50eaa1059d294476b28571a92
byeolyn <fatmaaakf@gmail.com>
2021-04-30 09:49:59 +0700

    Update tampilan dari page Registered User untuk Admin melihat akun terdaftar ref #1

--- adm_registereduser.php
+++ adm_registereduser.php
@@ -1,150 +1,200 @@
-<!doctype html>
+<?php
+  require_once "config.php";
+
+  session_start();
+
+  if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+  }
+
+  // buat query untuk ambil data dari database
+  $sql = "SELECT * FROM user WHERE id='".$_SESSION['id']."'";
+  $query = mysqli_query($link, $sql);
+  $user = mysqli_fetch_assoc($query);
+
+  // jika data yang di-edit tidak ditemukan
+  if( mysqli_num_rows($query) < 1 ){
+      die("Data tidak ditemukan...");
+  }
+?>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.82.0">
-    <title>Dashboard Template · Bootstrap v5.0</title>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Admin Dashboard</title>
+        <link href="bootstrap.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap5.min.css">
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
-
-    <!-- Bootstrap core CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-
-    <link href="dashboard.css" rel="stylesheet">
-  </head>
-  <body>
-    
-<header class="navbar navbar-light sticky-top bg-light flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
-    <img src="img/[logo].png" class="mx-auto d-block" alt="logo goedang" height="40">
-  </a>
-  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-  <ul class="navbar-nav px-3">
-    <li class="nav-item text-nowrap">
-      <a class="nav-link" href="#">Sign out</a>
-    </li>
-  </ul>
-</header>
-
-<div class="container-fluid">
-  <div class="row">
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="position-sticky pt-3">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="home"></span>
-              Dashboard
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">
-              <span data-feather="users"></span>
-              Data Pengguna
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file"></span>
-              Katalog
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="shopping-cart"></span>
-              Kategori Katalog
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="bar-chart-2"></span>
-              Informasi Cabang
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="layers"></span>
-              Profil
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
-    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Pengguna Terdaftar</h1>
-      </div>
-      <?php
-        require("config.php");
-        $sql = mysqli_query($config, "SELECT * FROM user");
-        $cek = mysqli_num_rows($sql);
-        if($cek==0){
-          echo '
-            <div class="container" style="margin-top: 70px; margin-bottom: 70px;">
-              <p class="lead" style="text-align: center;">No user data.</p>
+    </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-light bg-light">
+            <a class="navbar-brand" href="index.html"><img src="img/[logo].png" alt="" style="display: block;" class="mx-auto" height="42"></a>
+            <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+            <!-- Navbar Search-->
+            <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+                <div class="input-group">
+                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
+                    </div>
+                </div>
+            </form>
+            <!-- Navbar-->
+            <ul class="navbar-nav ml-auto ml-md-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fas fa-user fa-fw"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarScrollingDropdown">
+                      <li><a class="dropdown-item" href="#">Profil</a></li>
+                      <li><a class="dropdown-item" href="logout.php">Keluar</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-light" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Core</div>
+                            <a class="nav-link" href="index.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+                            <div class="sb-sidenav-menu-heading">User</div>
+                            <a class="nav-link" href="#">
+                                <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+                                Data User
+                            </a>
+                            <a class="nav-link" href="adm_profile.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-address-card"></i></div>
+                                Profil
+                            </a>
+                            <div class="sb-sidenav-menu-heading">Katalog</div>
+                            <a class="nav-link" href="adm_produk.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                                Produk
+                            </a>
+                            <a class="nav-link" href="adm_kategori.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Kategori
+                            </a>
+                            <a class="nav-link" href="cabang.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-store"></i></div>
+                                Cabang
+                            </a>
+                            <div class="sb-sidenav-menu-heading">Transaksi</div>
+                            <a class="nav-link" href="adm_produk.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-shopping-bag"></i></div>
+                                Pesanan
+                            </a>
+                            <a class="nav-link" href="adm_kategori.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-cash-register"></i></div>
+                                Pembayaran
+                            </a>
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Masuk sebagai:</div>
+                        <?php echo $user['nama'] ?>
+                    </div>
+                </nav>
             </div>
-          ';
-        } else{
-          echo '
-            <table class="table table-hover table-bordered">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Nama Lengkap</th>
-                  <th scope="col">E-mail</th>
-                  <th scope="col">No. Telp</th>
-                  <th scope="col">No. KTP</th>
-                  <th scope="col">Nama Toko</th>
-                  <th scope="col">No. Induk Toko</th>
-                  <th scope="col">Alamat Toko</th>
-                </tr>
-              </thead>
-              <tbody>
-            ';
-          while($tarik=mysqli_fetch_assoc($sql)){
-            echo '
-            <tr>
-              <th scope="row">'.$tarik['id'].'</th>
-              <td>'.$tarik['nama'].'</td>
-              <td>'.$tarik['email'].'</td>
-              <td>'.$tarik['notelp'].'</td>
-              <td>'.$tarik['nik'].'</td>
-              <td>'.$tarik['nama_toko'].'</td>
-              <td>'.$tarik['no_toko'].'</td>
-              <td>'.$tarik['alamat_toko'].'</td>
-            </tr>
-            ';
-          }
-          echo '
-            </tbody>
-            </table>
-          ';
-        }
-      ?>
-    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="dashboard.js"></script>
-  </body>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">User Terdaftar</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li class="breadcrumb-item active">User Terdaftar</li>
+                        </ol>
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-users mr-1"></i>
+                                User
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <?php
+                                        require("config.php");
+                                        $sql = mysqli_query($link, "SELECT * FROM user");
+                                        $cek = mysqli_num_rows($sql);
+                                        $i=1;
+                                        if($cek==0){
+                                        echo '
+                                            <div class="container" style="margin-top: 70px; margin-bottom: 70px;">
+                                            <p class="lead" style="text-align: center;">No user data.</p>
+                                            </div>
+                                        ';
+                                        } else{
+                                        echo '
+                                            <table class="table table-hover table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                            <thead>
+                                                <tr>
+                                                <th scope="col">#</th>
+                                                <th scope="col">Nama Lengkap</th>
+                                                <th scope="col">E-mail</th>
+                                                <th scope="col">No. Telp</th>
+                                                <th scope="col">NIK</th>
+                                                <th scope="col">Nama Toko</th>
+                                                <th scope="col">NIB</th>
+                                                <th scope="col">Alamat Toko</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                            ';
+                                        while($tarik=mysqli_fetch_assoc($sql)){
+                                            echo "
+                                            <tr>
+                                            <th scope='row'>".$i."</th>";
+                                            echo '
+                                            <td width="200">'.$tarik['nama'].'</td>
+                                            <td>'.$tarik['email'].'</td>
+                                            <td>'.$tarik['notelp'].'</td>
+                                            <td>'.$tarik['nik'].'</td>
+                                            <td width="110">'.$tarik['nama_toko'].'</td>
+                                            <td>'.$tarik['no_toko'].'</td>
+                                            <td>'.$tarik['alamat_toko'].'</td>
+                                            </tr>
+                                            ';
+                                            $i==$i++;
+                                        }
+                                        echo '
+                                            </tbody>
+                                            </table>
+                                        ';
+                                        }
+                                    ?>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Goedang 2021</div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+        <script src="scripts.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap5.min.js"></script>
+        <script src="datatables-demo.js"></script> 
+    </body>
 </html>
