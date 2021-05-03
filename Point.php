b05254c64418826d499d34681ae35e1ce7e3133e
MohDimasArifinMT <mohdimasarifin@student.telkomuniversity.ac.id>
2021-05-03 11:20:25 +0700

    Merapikan code ref #24

--- Point.php
+++ Point.php
@@ -11,8 +11,7 @@ if ($user_id == 1234 & $user_name == "dimas" & $id_belanja == 32) {
 }
 
 require_once "config.php";
-
-  session_start();
+session_start();
 
   if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
