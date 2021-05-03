4553fa756a61f84fd65abb4be265b233824f82e5
byeolyn <fatmaaakf@gmail.com>
2021-05-03 10:18:17 +0700

    Update code dan menyertakan Landing page ref #1

--- adm_registereduser.php
+++ adm_registereduser.php
@@ -27,7 +27,7 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Admin Dashboard</title>
-        <link href="bootstrap.css" rel="stylesheet" />
+        <link href="css/bootstrap.css" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap5.min.css">
@@ -192,9 +192,9 @@
 
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-        <script src="scripts.js"></script>
+        <script src="css/scripts.js"></script>
         <script type="text/javascript" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
         <script type="text/javascript" src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap5.min.js"></script>
-        <script src="datatables-demo.js"></script> 
+        <script src="css/datatables-demo.js"></script> 
     </body>
 </html>
